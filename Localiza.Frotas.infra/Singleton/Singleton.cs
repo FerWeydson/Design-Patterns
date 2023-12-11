710d3391bf91590b462e625c37091c67b5229b62
Weydson <weydfernandes@gmail.com>
2023-12-11 19:17:49 -0300

    Adicionar arquivos de projeto.

---
+++ Localiza.Frotas.infra/Singleton/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.infra.Singleton
+{
+    public sealed class Singleton
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+
+
+    }
+}
---
+++ Localiza.Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,18 @@
+﻿using Localiza.Frotas.infra.Singleton;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Localiza.Frotas.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var singleton = new Singleton();
+            return Ok(singleton);
+        }
+    }
+}
